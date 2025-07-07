1cbe463e3a8f314ba7966493fc073893ba367c4d
Brian Vance <briakvanc27@gmail.com>
2025-07-07 16:11:34 -0500

    generate utils lib

---
+++ libs/utils/jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+    displayName: 'utils',
+    preset: '../../jest.preset.cjs',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]s$': [
+            'ts-jest',
+            { tsconfig: '<rootDir>/tsconfig.spec.json' },
+        ],
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../coverage/libs/utils',
+};
